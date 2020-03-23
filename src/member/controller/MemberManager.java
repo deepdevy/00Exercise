515f4699e55373cda01338554c0489cd7266905c
yungi <yungi@218.38.137.28>
2020-03-23 20:19:57 +0900

    insertMember method 작성

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -3,6 +3,7 @@ package member.controller;
 import java.util.Scanner;
 
 import member.model.vo.Member;
+import member.view.MemberMenu;
 
 public class MemberManager {
 	
@@ -14,5 +15,27 @@ public class MemberManager {
 	
 	public void insertMember() {
 		
+		System.out.println("아이디 입력 : ");
+		String id = sc.next();
+		System.out.println("패스워드 입력 : ");
+		String pwd = sc.next();
+		System.out.println("이름 입력 : ");
+		String name = sc.next();
+		System.out.println("나이 입력 : ");
+		int age = sc.nextInt();
+		System.out.println("성별 입력 : ");
+		char gen = sc.next().charAt(0);
+		System.out.println("이메일 입력 : ");
+		String eml = sc.next();
+		
+		Member[] m = {
+				new Member(id, pwd, name, age, gen, eml),
+		};
+		count++;
+		
+		System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다.");
+		new MemberMenu().mainMenu();
 	}
+	
+	
 }
